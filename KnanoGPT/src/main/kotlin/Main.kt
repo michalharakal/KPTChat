8445b52f77e70fd83afea8a9dbbc84c2863a319b
Michal Harakal <michal.harakal@googlemail.com>
2024-03-22 22:42:22 +0100

    Embed docs from python commented repository and add support for diagrams.

--- KnanoGPT/src/main/kotlin/Main.kt
+++ KnanoGPT/src/main/kotlin/Main.kt
@@ -2,18 +2,17 @@ package de.jugda
 
 import de.jugda.de.jugda.knanogpt.core.data.ResourcesDataProvider
 import de.jugda.de.jugda.knanogpt.core.tensor.TrainTestSplitter
-import de.jugda.de.jugda.knanogpt.core.tensor.slice
-import de.jugda.knanogpt.core.tensor.Shape
-import jp.co.qoncept.tensorkotlin.Tensor
-import java.nio.file.Files
-import java.nio.file.Paths
 
 fun main() {
-    // Load & tokenize data
+    // Step 1. Import ..
+
+    // Step 2. Initialisierung-Block
+    // TBD
+
+    // Step 3. 4.  Load & tokenize data
     val dataProvider = ResourcesDataProvider("input.txt")
     val data = dataProvider.load()
-    println(data.slice(0, 999))
-    // slice data
+    // Step 5. Train & Test Spilt
     val splitter = TrainTestSplitter(data)
     val (train, test) = splitter.split(0.9f)
     println(train.shape.volume)
