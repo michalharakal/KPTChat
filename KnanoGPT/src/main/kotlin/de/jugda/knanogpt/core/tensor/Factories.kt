28abf9580550158820545b2b23642fef5a673d19
Michal Harakal <michal.harakal@googlemail.com>
2024-03-24 17:12:10 +0100

    Implement randint in Kotlin

---
+++ KnanoGPT/src/main/kotlin/de/jugda/knanogpt/core/tensor/Factories.kt
@@ -0,0 +1,17 @@
+package de.jugda.de.jugda.knanogpt.core.tensor
+
+import de.jugda.knanogpt.core.tensor.Shape
+import de.jugda.knanogpt.core.tensor.Tensor
+import kotlin.random.Random
+
+
+/**
+ * Returns a tensor filled with random integers generated uniformly between low (inclusive) and high (exclusive).
+ * The shape of the tensor is defined by the variable argument size.
+ */
+fun randint(random: Random, low: Int, high: Int, shape: Shape): Tensor {
+    require(high > low) { "High bound must be greater than low bound." }
+    val elements = FloatArray(shape.volume) { random.nextInt(low, high).toFloat() }
+    return Tensor(shape, elements)
+}
+
---
+++ KnanoGPT/src/test/kotlin/de/jugda/knanogpt/core/tensor/FactoriesKtTest.kt
@@ -0,0 +1,28 @@
+package de.jugda.knanogpt.core.tensor
+
+import de.jugda.de.jugda.knanogpt.core.data.ResourcesDataProvider
+import de.jugda.de.jugda.knanogpt.core.tensor.randint
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+
+class FactoriesTest {
+
+    @Test
+    fun testRandInt() {
+        val provider = ResourcesDataProvider("input.txt")
+        val tensor = provider.load()
+
+        val dataLen = tensor.elements.size
+        val blockSize = 8
+        val batchSize = 4 // # how many independent sequences will we process in parallel?
+        val random = Random(1337)
+        val randtensor = randint(random, 0, dataLen - blockSize, Shape(batchSize))
+        assertContentEquals(
+            randtensor.elements,
+            Tensor(Shape(batchSize), listOf(139827.0f, 146480.0f, 314577.0f, 99015.0f).toFloatArray()).elements
+        )
+    }
+
+}
