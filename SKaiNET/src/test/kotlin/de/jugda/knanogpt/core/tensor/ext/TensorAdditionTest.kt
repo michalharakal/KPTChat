23125ed2c7bb0709a2c18705c714ca235eca96f8
Michal Harakal <michal.harakal@googlemail.com>
2024-04-07 14:11:03 +0200

    Fix comment is assertion and add check for shape

--- SKaiNET/src/test/kotlin/de/jugda/knanogpt/core/tensor/ext/TensorAdditionTest.kt
+++ SKaiNET/src/test/kotlin/de/jugda/knanogpt/core/tensor/ext/TensorAdditionTest.kt
@@ -6,11 +6,12 @@ import de.jugda.knanogpt.core.tensor.Tensor
 
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
 
 class TensorAdditionTest {
     @Test
-    fun testSimpleMatrixAddition() {
+    fun testSimpleVectorAddition() {
         // Single element tensor should correctly calculate product as the element itself.
         val A = Tensor(Shape(1), listOf(1, 2, 3, 4).map { it.toDouble() }.toDoubleArray())
         val B = Tensor(Shape(1), listOf(5, 6, 7, 8).map { it.toDouble() }.toDoubleArray())
@@ -18,7 +19,23 @@ class TensorAdditionTest {
         assertContentEquals(
             C.elements,
             (A + B).elements,
-            "Product of a single-element tensor should be the element itself"
+            "Product of a simple tensor addition should be a tensor with elements 6, 8, 10, 12"
         )
+        assertEquals(A.shape, C.shape)
     }
+
+    @Test
+    fun testSimpleMatrixAddition() {
+        // Single element tensor should correctly calculate product as the element itself.
+        val A = Tensor(Shape(2, 2), listOf(1, 2, 3, 4).map { it.toDouble() }.toDoubleArray())
+        val B = Tensor(Shape(2, 2), listOf(5, 6, 7, 8).map { it.toDouble() }.toDoubleArray())
+        val C = Tensor(Shape(2, 2), listOf(6, 8, 10, 12).map { it.toDouble() }.toDoubleArray())
+        assertContentEquals(
+            C.elements,
+            (A + B).elements,
+            "Product of a simple tensor addition should be a tensor with elements 6, 8, 10, 12"
+        )
+        assertEquals(A.shape, C.shape)
+    }
+
 }
