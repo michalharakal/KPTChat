70770451633732dcf0fa4824c14140355c82fe64
Michal Harakal <michal.harakal@googlemail.com>
2024-09-05 15:34:52 +0200

    Add loading of content into model

--- SKaiNET-mobile/build.gradle.kts
+++ SKaiNET-mobile/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "sk.net.ai"
-version = "0.0.1"
+version = "0.0.2"
 
 kotlin {
     jvmToolchain(17)
--- SKaiNET-mobile/src/commonMain/kotlin/nn/KGPT.kt
+++ SKaiNET-mobile/src/commonMain/kotlin/nn/KGPT.kt
@@ -1,11 +1,15 @@
 package nn
 
-class KGPT {
+class KGPT(modelIndex:Int) {
     fun generate(from: String, maxNewTokens: Int): String {
         val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9') + ' '
 
         return (1..maxNewTokens)
             .map { charset.random() }
             .joinToString("")
+    }
+
+    fun loadModelContent(content:String) {
+
     }
 }
