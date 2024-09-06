416e90e0c6da3a6c1f887ed7843b5f9c42387a27
Michal Harakal <michal.harakal@googlemail.com>
2024-09-06 12:51:38 +0200

    Load fake shakespear model

--- SKaiNET-mobile/build.gradle.kts
+++ SKaiNET-mobile/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "sk.net.ai"
-version = "0.0.2"
+version = "0.0.3"
 
 kotlin {
     jvmToolchain(17)
--- SKaiNET-mobile/src/commonMain/kotlin/nn/KGPT.kt
+++ SKaiNET-mobile/src/commonMain/kotlin/nn/KGPT.kt
@@ -1,15 +1,40 @@
 package nn
 
-class KGPT(modelIndex:Int) {
-    fun generate(from: String, maxNewTokens: Int): String {
-        val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9') + ' '
+import kotlin.random.Random
 
-        return (1..maxNewTokens)
-            .map { charset.random() }
-            .joinToString("")
+class KGPT(modelIndex: Int) {
+    var loaded = false
+    val shakespeare = """"O gentle moon, that sails the velvet skies,
+Thy silver beams doth grace the lover's path.
+Yet fickle Fortune, with her cruel hand,
+Doth turn the wheel and scatter dreams to dust.
+Why doth the heart, so filled with tender hope,
+Beat on in sorrow when the light is lost?
+For love, though pure, doth tremble at the storm,
+And yields to shadows creeping on the soul.
+Yet, in the darkest hour, shall truth arise,
+And from despair, sweet joy shall be reborn.
+For time doth heal the deepest wound,
+And in its course, the broken heart finds peace,
+Restoring faith in love once more,
+When dawn returns to chase the night away.""""
+
+    fun generate(from: String, maxNewTokens: Int): String {
+        return if (loaded) {
+            val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9') + ' '
+
+            return (1..maxNewTokens)
+                .map { charset.random() }
+                .joinToString("")
+        } else {
+            val range = shakespeare.indices// Define your range
+            val randomIndex = Random.nextInt(range.first, range.last) // Generate random index
+
+            shakespeare.substring(randomIndex, randomIndex + maxNewTokens)
+        }
     }
 
-    fun loadModelContent(content:String) {
-
+    fun loadModelContent(content: String) {
+        loaded = true
     }
 }
